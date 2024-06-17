e8c6df404859b4cd1a89633e45a89f5b1012404d
scuba75 <scuba75@gmail.com>
2024-06-17 11:37:54 -0500

    update

--- src/inventory/journey/index.js
+++ src/inventory/journey/index.js
@@ -16,11 +16,15 @@ module.exports = async( {info = {}, material = {}})=>{
     html += '</head>'
     html += '<body>'
       html += '<table padding="0" width="700px">'
-        html += '<tr class="header-text"><td colspan="3">'+info.player+' '+info.nameKey+' Journey Guide Gear and relic mats needed</td></tr>'
-
+        html += '<tr class="header-text"><td colspan="3">'+info.player+' '+info.nameKey
+        if(info.isJourneyGuide) html += ` Journey Guide`
+        if(!info.isJourneyGuide) html += ` squad`
+        html += ` Gear and relic mats needed</td></tr>`
+        if(info.includeGuideUnit) html += `<tr class="${bkImg}"><td colspan=3 class="gear-name">Includes needed gear/relic material for ${info.nameKey} to R5</td></tr>`
         //html += '<tr class="header-text"><td><div style="margin-left: 10px" class="character-portrait__image-frame character-portrait__image-frame--size-small"><img class="character-portrait__img character-portrait__img--size-small" src="/thumbnail/'+uInfo.thumbnailName+'.png"></div></td><td colspan="2" align="left">'+(info.header || uInfo.nameKey)+'</td></tr>'
         if(gear?.length > 0){
           html += '<tr class="'+bkImg+'"><td colspan="3" class="gear-name">Additional Gear needed</td></tr>'
+
           if(info.includeInventory) html += '<tr class="'+bkImg+'"><td colspan=3 class="gear-name">Note: gear only looks at salvage, not crafted gear/components for what is in inventory</td></tr>'
           oddCount++
           for(let i in gear){

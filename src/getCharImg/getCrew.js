c737fef367da74bf522f8c488a277fe035654d12
scuba75 <scuba75@gmail.com>
2024-09-25 11:49:06 -0500

    update

--- src/getCharImg/getCrew.js
+++ src/getCharImg/getCrew.js
@@ -7,15 +7,23 @@ module.exports = (obj = {})=>{
           html += '<div class="character-portrait__image-frame character-portrait__image-frame--size-micro">'
             html += '<img class="character-portrait__img character-portrait__img--size-micro" src="/thumbnail/'+obj.thumbnailName+'.png"/>'
           html += '</div>'
+          let unitAlignment = (obj.alignment == 'light' ? 2:3)
+          if(obj.ultimate) unitAlignment += ' character-portrait__relic--ultimate'
           if(obj?.relic > 0){
-            let unitAlignment = (obj.alignment == 'light' ? 2:3)
-            if(obj.ultimate) unitAlignment += ' character-portrait__relic--ultimate'
+
             html += '<div class="character-portrait__relic character-portrait__relic--size-micro character-portrait__relic--alignment-'+unitAlignment+'">'+(+obj.relic - 2)+'</div>'
             html += '<div class="character-portrait__rframe character-portrait__rframe--size-micro character-portrait__rframe--alignment-'+unitAlignment+'"></div>'
             html += '<div class="character-portrait__rframe  character-portrait__rframe--right character-portrait__rframe--size-micro character-portrait__rframe--alignment-'+unitAlignment+'"></div>'
           }else{
-            html += '<div class="character-portrait__gframe character-portrait__gframe--size-micro character-portrait__gframe--tier-'+obj.gear+'"></div>'
-            html += '<div class="character-portrait__level character-portrait__level--size-micro">'+obj.level+'</div>'
+            if(obj.gear == 13){
+              html += '<div class="character-portrait__level character-portrait__level--size-micro">'+obj.level+'</div>'
+              html += '<div class="character-portrait__rframe character-portrait__rframe--size-micro character-portrait__rframe--alignment-'+unitAlignment+'"></div>'
+              html += '<div class="character-portrait__rframe  character-portrait__rframe--right character-portrait__rframe--size-micro character-portrait__rframe--alignment-'+unitAlignment+'"></div>'
+
+            }else{
+              html += '<div class="character-portrait__gframe character-portrait__gframe--size-micro character-portrait__gframe--tier-'+obj.gear+'"></div>'
+              html += '<div class="character-portrait__level character-portrait__level--size-micro">'+obj.level+'</div>'
+            }
           }
           if(obj.zeta) html += '<div class="character-portrait__zeta character-portrait__zeta--size-micro">'+obj.zeta+'</div>'
           if(obj.omi) html += '<div class="character-portrait__omicron character-portrait__omicron--size-micro">'+obj.omi+'</div>'
